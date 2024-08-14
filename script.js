37d84bfa276fffdafca4a05be4a09159abfd482c
Zainab_Naz <arizakhanarizakhan869@gmail.com>
2024-08-14 13:25:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// <!-- chapter #03 -->
+
+
+
+var name = "John Doe";
+
+var title= "T-shirts";
+
+var quantity = 10;
+
+document.write(_name+ " ordered "+ _quantity +" +_title+ on XYZ Clothing.");
